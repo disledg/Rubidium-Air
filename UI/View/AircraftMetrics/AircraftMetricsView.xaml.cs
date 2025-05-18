2f7e99076f4e26ad4dcbdb7d45bfd034f9439cdf
Spacerazer <e2gmodprofi@gmail.com>
2025-05-18 21:43:25 +0300

    Интерфейс для параметров ВС

---
+++ UI/View/AircraftMetrics/AircraftMetricsView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Rubidium
+{
+    public partial class AircraftMetricsView : UserControl
+    {
+        public AircraftMetricsView()
+        {
+            InitializeComponent();
+        }
+    }
+} 
---
+++ UI/View/AircraftMetrics/EditAircraftMetricsView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace Rubidium
+{
+    public partial class EditAircraftMetricsView : Window
+    {
+        public EditAircraftMetricsView()
+        {
+            InitializeComponent();
+        }
+    }
+} 
