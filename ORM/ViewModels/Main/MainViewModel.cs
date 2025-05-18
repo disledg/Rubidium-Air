dc769f87ca03438ebe5df17a757c8aab3bf6bc4e
Spacerazer <e2gmodprofi@gmail.com>
2025-05-18 21:37:06 +0300

    Обновление внутрянки главного меню для оторажения новых модулей

--- ORM/ViewModels/Main/MainViewModel.cs
+++ ORM/ViewModels/Main/MainViewModel.cs
@@ -16,24 +16,35 @@ namespace Rubidium
         private readonly FlightService _flightService;
         private readonly BaggageService _baggageService;
         private readonly EmployeeService _employeeService;
+        private readonly AircraftService _aircraftService;
+        private readonly AircraftMetricsService _aircraftMetricsService;
 
         public ICommand NavigateToFlightsCommand { get; }
         public ICommand NavigateToEmployeesCommand { get; }
         public ICommand NavigateToBaggageCommand { get; }
+        public ICommand NavigateToAircraftCommand { get; }
+        public ICommand NavigateToAircraftMetricsCommand { get; }
 
-        public MainViewModel(NavigationService navigation, FlightService flightService, EmployeeService employeeService, BaggageService baggageService)
+        public MainViewModel(NavigationService navigation, FlightService flightService, EmployeeService employeeService, BaggageService baggageService, AircraftService aircraftService, AircraftMetricsService aircraftMetricsService)
         {
             _navigation = navigation;
             _flightService = flightService;
             _employeeService = employeeService;
             _baggageService = baggageService;
+            _aircraftService = aircraftService;
+            _aircraftMetricsService = aircraftMetricsService;
             _navigation.RegisterPage("Flights", () => new FlightsViewModel(flightService));
             _navigation.RegisterPage("Employees", () => new EmployeesViewModel(employeeService));
             _navigation.RegisterPage("Baggage", () => new BaggageViewModel(baggageService));
+            _navigation.RegisterPage("Aircraft", () => new AircraftViewModel(aircraftService));
+            _navigation.RegisterPage("AircraftMetrics", () => new AircraftMetricsViewModel(aircraftMetricsService, aircraftService));
+
 
             NavigateToFlightsCommand = new RelayCommand(_ => NavigateTo("Flights"));
             NavigateToEmployeesCommand = new RelayCommand(_ => NavigateTo("Employees"));
             NavigateToBaggageCommand = new RelayCommand(_ => NavigateTo("Baggage"));
+            NavigateToAircraftCommand = new RelayCommand(_ => NavigateTo("Aircraft"));
+            NavigateToAircraftMetricsCommand = new RelayCommand(_ => NavigateTo("AircraftMetrics"));
 
             NavigateTo("Flights");
         }
